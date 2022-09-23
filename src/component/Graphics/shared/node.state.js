707cda057beb9b8df06ed8be9e232db5afbc5dac
maggot_code <wowhuanhuan233@163.com>
2022-09-23 16:33:45 +0800

    fix :-1:  add node state

--- src/component/Graphics/shared/node.state.js
+++ src/component/Graphics/shared/node.state.js
@@ -3,31 +3,37 @@
  * @Author: maggot-code
  * @Date: 2022-08-24 15:16:19
  * @LastEditors: maggot-code
- * @LastEditTime: 2022-08-29 14:52:34
+ * @LastEditTime: 2022-09-23 16:28:07
  * @Description: 
  */
+const startPending = {
+    toReach: true,
+    toContent: false,
+    className: "node-start-pending"
+};
 const finishState = {
     toReach: true,
-    toRemarks: false,
+    toContent: false,
     className: "node-finish"
 };
 const pendingState = {
     toReach: true,
-    toRemarks: false,
+    toContent: false,
     className: "node-pending"
 };
 const refuseState = {
     toReach: true,
-    toRemarks: true,
+    toContent: true,
     className: "node-refuse"
 };
 const unknownState = {
     toReach: false,
-    toRemarks: false,
+    toContent: false,
     className: "node-unknown"
 };
 
 export const StateContext = {
+    startpending: startPending,
     finish: finishState,
     pending: pendingState,
     refuse: refuseState,
